994a3cf24dbf45c1b14f578a065507fd62fb9e7d
Daniel Aliu <danielaliu202@gmail.com>
2023-07-04 22:50:18 +0100

    Hint button functionality

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+// Creating user info
+
 let userInfo = {
     "level": 1,
     "hintsUsed": 0,
 }
 
-const emojiContainer = document.getElementById("emojiDisplay")
-const emojisHolder = document.createElement("div")
+// Setting important variables for the document
 
+const emojiContainer = document.getElementById("emojiDisplay")
+const hintBtn = document.getElementById("hint-btn")
+
+// Setting loading the images for each level based on the user info
 
 function loadLevel (){
   document.getElementById("emojiDisplay").innerHTML = "";
@@ -21,9 +26,38 @@ function loadLevel (){
     .catch(error => {
       console.log(error);
     });
-  
-    let level1 = {
-      "answer": "cowboy hat",
-      "hints": ["Yee Haw!", "Er..Cowboys wear them?"],
-  }
 }
+
+// Loading the initial level
+
+loadLevel()
+
+// Showing hints based on the level they are on and the amount of hints used
+
+function showHint() {
+  fetch(`/levelinfo/level${userInfo["level"]}-info.json`)
+    .then(response => response.json())
+    .then(data => {
+      if(`${userInfo["hintsUsed"]}`< 3) {
+        let hintBox = document.createElement("div")
+        let hintImg = document.createElement("img")
+        let hintText = document.createElement("span")
+  
+        hintBox.classList.add("hints")
+  
+        hintImg.src = "/assets/misc/Hintimg.png"
+  
+        hintText.innerHTML = `Hint: ${data["hints"][userInfo["hintsUsed"]]}`
+    
+        hintBox.appendChild(hintImg)
+        hintBox.appendChild(hintText)
+        document.getElementById("hint-box").appendChild(hintBox)
+
+        userInfo["hintsUsed"]+= 1
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
